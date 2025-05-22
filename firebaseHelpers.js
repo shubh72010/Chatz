9da05cb690f1b8c18e7285d5475a3df084af8cd7
shubh72010 <108581630+shubh72010@users.noreply.github.com>
2025-05-22 15:02:38 +0530

    Update firebaseHelpers.js

--- firebaseHelpers.js
+++ firebaseHelpers.js
@@ -17,13 +17,13 @@ import {
   get,
 } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-database.js";
 
-// Your Firebase config — fixed storageBucket URL
+// Your fixed Firebase config
 const firebaseConfig = {
   apiKey: "AIzaSyAv4mVF8Y8lEKNK1vhBTy2Nj2Ya3l7ZJyQ",
   authDomain: "chatz-45df4.firebaseapp.com",
   databaseURL: "https://chatz-45df4-default-rtdb.firebaseio.com",
   projectId: "chatz-45df4",
-  storageBucket: "chatz-45df4.appspot.com",  // fixed this line
+  storageBucket: "chatz-45df4.appspot.com", // fixed storage bucket URL
   messagingSenderId: "463847844545",
   appId: "1:463847844545:web:5006247d061c3e0dc28240",
   measurementId: "G-2VHETC9V8B",
