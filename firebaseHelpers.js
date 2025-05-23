e33d3f111390573f7ff07eb0f79e31a87da76469
shubh72010 <108581630+shubh72010@users.noreply.github.com>
2025-05-23 17:28:15 +0530

    Update firebaseHelpers.js

--- firebaseHelpers.js
+++ firebaseHelpers.js
@@ -1,14 +1,11 @@
-// firebaseHelpers.js
-
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-app.js";
 import {
   getAuth,
   GoogleAuthProvider,
   signInWithPopup,
   signOut,
-  onAuthStateChanged,
+  onAuthStateChanged
 } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-auth.js";
-
 import {
   getFirestore,
   collection,
@@ -19,15 +16,13 @@ import {
   doc,
   setDoc,
   getDoc,
-  where,
-  serverTimestamp, // <-- Added this
+  where
 } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-firestore.js";
 
-// Your Firebase config
+// Only this is for Firestore, not RTDB
 const firebaseConfig = {
   apiKey: "AIzaSyAv4mVF8Y8lEKNK1vhBTy2Nj2Ya3l7ZJyQ",
   authDomain: "chatz-45df4.firebaseapp.com",
-  databaseURL: "https://chatz-45df4-default-rtdb.firebaseio.com",
   projectId: "chatz-45df4",
   storageBucket: "chatz-45df4.appspot.com",
   messagingSenderId: "463847844545",
@@ -35,13 +30,12 @@ const firebaseConfig = {
   measurementId: "G-2VHETC9V8B"
 };
 
-// Init Firebase
 const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
-const db = getFirestore(app);
-const provider = new GoogleAuthProvider();
 
-// Export everything you need
+const auth = getAuth(app);
+const provider = new GoogleAuthProvider();
+const db = getFirestore(app);
+
 export {
   auth,
   db,
@@ -57,6 +51,5 @@ export {
   doc,
   setDoc,
   getDoc,
-  where,
-  serverTimestamp, // <-- Added this to export
+  where
 };
