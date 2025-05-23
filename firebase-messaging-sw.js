b5fdcb513f1e0f08eedf9fb38bed0edef1c6fd3a
shubh72010 <108581630+shubh72010@users.noreply.github.com>
2025-05-23 10:48:34 +0530

    Update firebase-messaging-sw.js

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -3,11 +3,14 @@ importScripts("https://www.gstatic.com/firebasejs/10.11.0/firebase-app-compat.js
 importScripts("https://www.gstatic.com/firebasejs/10.11.0/firebase-messaging-compat.js");
 
 firebase.initializeApp({
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT.firebaseapp.com",
-  projectId: "YOUR_PROJECT",
-  messagingSenderId: "YOUR_SENDER_ID",
-  appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyAv4mVF8Y8lEKNK1vhBTy2Nj2Ya3l7ZJyQ",
+  authDomain: "chatz-45df4.firebaseapp.com",
+  databaseURL: "https://chatz-45df4-default-rtdb.firebaseio.com",
+  projectId: "chatz-45df4",
+  storageBucket: "chatz-45df4.firebasestorage.app",
+  messagingSenderId: "463847844545",
+  appId: "1:463847844545:web:5006247d061c3e0dc28240",
+  measurementId: "G-2VHETC9V8B"
 });
 
 const messaging = firebase.messaging();
