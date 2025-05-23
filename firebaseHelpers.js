40b54b16084191a341314ac02be4c48a0d401c49
shubh72010 <108581630+shubh72010@users.noreply.github.com>
2025-05-23 15:48:49 +0530

    Update firebaseHelpers.js

--- firebaseHelpers.js
+++ firebaseHelpers.js
@@ -8,6 +8,7 @@ import {
   signOut,
   onAuthStateChanged,
 } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-auth.js";
+
 import {
   getFirestore,
   collection,
@@ -19,25 +20,28 @@ import {
   setDoc,
   getDoc,
   where,
+  serverTimestamp, // <-- Added this
 } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-firestore.js";
 
+// Your Firebase config
 const firebaseConfig = {
   apiKey: "AIzaSyAv4mVF8Y8lEKNK1vhBTy2Nj2Ya3l7ZJyQ",
   authDomain: "chatz-45df4.firebaseapp.com",
   databaseURL: "https://chatz-45df4-default-rtdb.firebaseio.com",
   projectId: "chatz-45df4",
-  storageBucket: "chatz-45df4.firebasestorage.app",
+  storageBucket: "chatz-45df4.appspot.com",
   messagingSenderId: "463847844545",
   appId: "1:463847844545:web:5006247d061c3e0dc28240",
   measurementId: "G-2VHETC9V8B"
 };
 
+// Init Firebase
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const db = getFirestore(app);
-
 const provider = new GoogleAuthProvider();
 
+// Export everything you need
 export {
   auth,
   db,
@@ -54,4 +58,5 @@ export {
   setDoc,
   getDoc,
   where,
+  serverTimestamp, // <-- Added this to export
 };
